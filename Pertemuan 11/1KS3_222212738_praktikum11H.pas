8f9678cc4ccb6d7d02930f4c709e04f6a0d7dd45
Muh. Nur Afrizal <muhamadnurafrizal5@gmail.com>
2023-06-03 13:33:01 +0700

    penugasan 11 fix

--- Pertemuan 11/1KS3_222212738_praktikum11H.pas
+++ Pertemuan 11/1KS3_222212738_praktikum11H.pas
@@ -2,11 +2,20 @@ program Penugasan3;
 uses crt, sysutils;
 
 Function findBiner(input: integer): string;
+    var
+        input_positif: word;
+    
     begin
         if input = 1 then
             findBiner := '1'
-        else
-            findBiner := findBiner(input div 2) + IntToStr(input mod 2);
+        else if input = -1 then
+            findBiner := '-1'
+        else begin
+            input_positif := input;
+            if input < 0 then
+                input_positif *= -1;
+            findBiner := findBiner(input div 2) + IntToStr(input_positif mod 2);
+        end;
     end;
 
 var
